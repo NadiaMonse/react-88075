1db6cd88268eb7569d5d44172c3f9d5ca9f15109
nadiamonse <nadia.monserrat89@gmail.com>
2025-09-26 23:50:02 -0300

    primera entrega

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+const CartWidget =()=>{
+    return (
+        <div style={{fontSize:"20px"}}>
+            <FaShoppingCart/>
+        </div>
+    );
+};
+export default CartWidget;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+
+import CartWidget from "./CartWidget";
+
+
+export default function NavBar(){
+    return(
+        <nav style={styles.nav}>
+            <h2 style={styles.logo}> Mi Tienda</h2>
+            <ul style={styles.menu}>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+            </ul>
+            <CartWidget/>
+    </nav>
+    );
+};
+
+const styles={
+    nav:{
+        display:"flex",
+        justifyContent: "space-between",
+        alignItems:"center",
+        padding:"10px 20px",
+        backgroundColor:"#e935daff",
+        color:"white"
+    },
+    logo:{margin:0},
+    menu:{display:"flex",listStyle:"none",gap:"15px"}
+};
